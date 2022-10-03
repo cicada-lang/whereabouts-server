96ee1aa8507fd8ea058ba3bdea7ddc76bd03b37a
Xie Yuheng <xyheme@gmail.com>
2022-10-03 19:31:34 +0800

    up

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
 const app = require("express")()
+const Whereabouts = require("@cicada-lang/cicada-whereabouts")
 
-app.get("*", (req, res) => {
+app.get("/hi", (req, res) => {
   res.setHeader("Content-Type", "text/html")
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate")
   res.end("Hello world!")
+})
+
+app.get("/run", (req, res) => {
+  res.setHeader("Content-Type", "text/html")
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate")
+  res.end("Whereabouts!")
 })
 
 module.exports = app
